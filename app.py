86a7af26dd40388ab4383eb9d31423cb0e389715
Nour <nooralian631@gmail.com>
2026-03-05 21:51:13 +0300

    update 3

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import re
 import random
+import os
 
 app = Flask(__name__)
 
@@ -178,5 +179,6 @@ def chat():
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
